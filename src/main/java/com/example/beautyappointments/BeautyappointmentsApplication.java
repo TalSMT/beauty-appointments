fb41298fa7209d90db4a1bd0bbbaee0fcdd4e3f8
TalSMT <talooshkosh@gmail.com>
2026-05-27 09:37:13 +0300

    Connected Spring Boot to MySQL

---
+++ src/main/java/com/example/beautyappointments/BeautyappointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.beautyappointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyappointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyappointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beautyappointments/BeautyappointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beautyappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyappointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
